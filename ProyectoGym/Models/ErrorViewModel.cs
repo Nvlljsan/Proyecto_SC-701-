f2275bf7b106cdaea9f2b2989bb1a7274efd55ff
JuanD Sanchez <jd.sanchez.gamboa@gmail.com>
2024-10-02 11:14:41 -0600

    Add project files.

---
+++ ProyectoGym/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoGym.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
