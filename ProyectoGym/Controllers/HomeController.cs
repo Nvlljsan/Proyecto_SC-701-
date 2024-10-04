1768102ad4df218fefc573e5a03a1c156d88d452
leonel <98724070+Lazofeifa@users.noreply.github.com>
2024-10-04 13:41:40 -0600

    Recuperar clave
    Vista de recuperar clave,
    layout externo

--- ProyectoGym/Controllers/HomeController.cs
+++ ProyectoGym/Controllers/HomeController.cs
@@ -28,5 +28,11 @@ namespace ProyectoGym.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult RecuperarClave()
+        {
+            return View();
+        }
+
     }
 }
